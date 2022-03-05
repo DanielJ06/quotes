a431ef0cd7d37698bd77677eb47d05d41b2b34e6
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-03-05 12:22:12 -0300

    Move bookmark dispatcher

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import QuoteCard from "../../components/QuoteCard";
+
+import { removeQuote } from "../../store/modules/bookmark/actions";
+import { Quote } from "../../types/quote";
 import { RootState } from "../../types/RootState";
 
 import * as S from "./styles";
 
 const Favorites: React.FC = () => {
+	const dispatch = useDispatch();
 	const { quotes } = useSelector((state: RootState) => state.bookmark);
+
+	const handleRemoveBookmark = (quote: Quote) => {
+		dispatch(removeQuote(quote));
+	};
 
 	return (
 		<S.Container>
 			<FlatList
 				data={quotes}
 				keyExtractor={(q) => q.quote}
-				renderItem={({ item }) => <QuoteCard quote={item} />}
+				renderItem={({ item }) => (
+					<QuoteCard
+						longPressCb={(q) => handleRemoveBookmark(q)}
+						quote={item}
+					/>
+				)}
 			/>
 		</S.Container>
 	);
